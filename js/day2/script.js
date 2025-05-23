a09173dc0c8620134b9faaefb5065e3614597236
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-05-23 20:01:28 +0530

    update day 2

---
+++ js/day2/script.js
@@ -0,0 +1,129 @@
+
+
+
+
+// let = red 
+// var = green 
+// const = blue
+
+// const  name = "Besant Technologies"//string
+// var age = 23 //number
+// var isStudent = true //boolean 
+
+// //nul 
+// //undefined
+
+// let hobbies = ["reading", "gaming", "coding"] //array
+// let address = {
+//     city: "Bangalore",
+//     state: "Karnataka",
+//     country: "India"
+// } //object
+
+
+
+// alert("hello" + name)
+
+
+
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+// console.log(hobbies);
+
+
+
+
+
+
+
+const ar = "Arun from besant technologies";
+
+
+// console.log(typeof ar);
+
+
+
+let age = 25;
+let price = 99.99;
+// console.log(typeof age + price);
+
+
+
+
+let bigNumber = 1234567890123456789012345678901234567890786387293n;
+// console.log(typeof bigNumber);
+
+
+
+let isStudent = true;
+let isAdmin = false;
+
+// console.log(typeof isStudent);
+// console.log(typeof isAdmin);
+
+
+
+let score;// console.log(typeof score);
+score = 100;
+
+// console.log(score);
+
+
+
+let user = null;
+// console.log(user);
+
+
+// console.log(typeof user);
+
+
+
+let id = Symbol("id");
+let id2 = Symbol("id");
+// console.log(id === id2);
+
+
+let person = {}; 
+
+
+console.log(person.name);
+
+
+
+
+
+
+
+// const car = "hundai";
+// const bike = "honda";
+// const bus = "volvo";
+// const truck = "tata";
+// const cycle = "hero";
+// const train = "indian railways";
+// const airplane = "boeing";
+
+// const persons = {
+//     name: "Arun",
+//     age: 25,
+//     isStudent: true,
+//     hobbies: ["reading", "gaming", "coding"],
+//     address: {
+//         city: "Bangalore",
+//         state: "Karnataka",
+//         country: "India"
+//     },
+//     vehicle : {
+//         car: "hundai",
+//         bike: "honda",
+//         bus: "volvo",
+//         truck: "tata",
+//         cycle: "hero",
+//         train: "indian railways",
+//         airplane: "boeing"
+//     }
+// }
+
+
+
+// console.log(persons.vehicle);
