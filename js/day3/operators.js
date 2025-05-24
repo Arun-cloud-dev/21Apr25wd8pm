89f87d28939f1629e5e6c36d11b0fa9e68567929
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-05-24 19:48:57 +0530

    day 3

---
+++ js/day3/operators.js
@@ -0,0 +1,67 @@
+// maths  Arithmetic Operators
+// +, -, *, /, %, ** (exponentiation)
+
+// const a = 10, b = 3;
+
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b); 
+
+
+// Arithmetic Operators 
+
+
+// let x = 5;
+
+// x += 3;
+//  // x = x(5) + 3 -> x = 8;
+// console.log(x);
+
+// x *= 2;
+// // x = x(8) * 2 -> x = 16;
+
+
+
+
+// console.log(x);
+
+
+//comparison operators
+// ==, ===, !=, !==, >, <, >=, <=
+
+// == checks for value equality
+// console.log(5 == "5"); // true
+// // === checks for value and type equality
+// console.log(5 === "5"); // false
+// // != checks for value inequality
+// console.log(5 != "5"); // false
+// // !== checks for value and type inequality
+// console.log(5 !== "5"); // true
+// // > checks if left operand is greater than right operand
+// console.log(5 > 3); // true
+// // < checks if left operand is less than right operand
+// console.log(5 < 3); // false
+// // >= checks if left operand is greater than or equal to right operand
+// console.log(5 >= 5); // true
+// // <= checks if left operand is less than or equal to right operand
+// console.log(5 <= 3); // false
+
+
+
+// // Logical Operators
+// console.log(5 & 1);     0101 & 0001   1
+
+// console.log(1 | 1);  0001 | 0001 
+
+// 
+// NOT
+console.log(!false);
+
+
+
+
+
---
+++ js/day3/variables.js
@@ -0,0 +1,142 @@
+// x = 5;
+// y = 6;
+
+// z = x + y ;
+
+// console.log(z);
+
+
+
+// var x = 5;
+
+// var y = 6;
+
+// var z = x + y;
+// console.log(z);
+
+
+
+// let x = 5;
+// let y = 6;
+// let z = x + y;
+
+// console.log(z);
+
+
+
+
+
+// const x1 = 5;
+// const y1 = 6;
+// const z1= x1 + y1;
+// console.log(z1);
+
+
+// variable sthat are declared with let  have a block scope
+// variables declared with let must be declared before they are used
+
+// variables that are declared with let cannot be redeclared
+
+
+
+
+
+
+
+// {
+//     let x = 5;
+//     console.log(x);
+// }
+
+// let y = 6;
+
+// console.log(y);
+// // console.log(x);
+// let a;
+// a = 5;
+
+
+// console.log(a);
+
+
+
+
+
+
+
+//redeclaring cannt be done with let
+
+
+// let x = 5;
+
+// {
+//     let x = 0;
+// }
+
+// var x = "jo"
+//  console.log(x);
+// var x = 0;
+// var x = 5;
+
+// console.log(x);
+
+
+// var x = 10 ;
+
+
+// {
+//     var x = 2
+// }
+
+
+// console.log(x);
+
+
+
+
+// Variables defined with const cannot be Redeclared
+
+// const PI = 3.57689090; 
+
+// {
+//     const PI = 3.14;
+// }
+
+// console.log(PI);
+
+
+// variables must be assigned a value when they are declared:
+
+
+// const x = 5;
+
+
+
+// console.log(x);
+
+// Variables defined with const cannot be Reassigned
+
+// Variables defined with const have Block Scope
+
+// should be changed use let 
+
+// should not change use const
+    // WHEN AND =WHERE  CONST
+// A new Array
+// const cars = ["Saab", "Volvo", "BMW"];
+// A new Object
+// const car = {
+//     type: "Fiat",
+//     model: "500",
+//     color: "white"
+// };
+
+// A new Function
+// A new RegExp
+
+
+// .this
+
+
+
+
