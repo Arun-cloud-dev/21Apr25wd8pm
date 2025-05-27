e0ca2a11b6751bc098ca921376e0b6ce3e508278
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-05-27 19:44:06 +0530

    update day5

---
+++ js/day5/Loops.js
@@ -0,0 +1,52 @@
+//for Loop 
+
+
+
+// for (let i = 0; i <= 5; i++){
+//     console.log(i);
+// }
+
+
+//while Loop syntax
+// while (condition) {
+//     // code block
+// }
+// let i = 0;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+
+
+//do...while Loop 
+
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 0);
+
+
+
+//for ...in Lopp 
+
+
+
+// const student = {name: "John", age: 20, grade: "A"};
+
+
+// for (let ey in student) {
+//     console.log(ey + "?" + student[ey]);
+// }
+
+// console.log(student);
+
+
+
+
+const furits = ["apple", "banana", "cherry", "date"];
+for (let key in furits) {
+    console.log(key + ":" + furits[key]);
+}
---
+++ js/day5/functions.js
@@ -0,0 +1,35 @@
+// function ar() {
+//    console.log("hello , students !");;
+// }
+// ar();
+
+
+
+// function add(a, b) {
+//     return a +b 
+// }
+// let result = add(10, 40)
+// console.log("sum:", result);
+
+
+
+
+// //Arrow function syntax
+// let multiply = (x, y) => x * y;
+// console.log("Product is :" , multiply(5, 10));
+
+
+
+
+// function greet(name, calback) {
+//     console.log("Hello " + name + "!");
+//     calback();
+// }
+
+
+// function farewell() {
+//     console.log("Goodbye!");
+// }
+
+// greet("Alice", farewell);
+
