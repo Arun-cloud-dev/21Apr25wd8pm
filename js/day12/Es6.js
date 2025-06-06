ea777f83e36c255600305c4683f41816f307f956
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-06-06 19:58:27 +0530

    update

---
+++ js/day12/Es6.js
@@ -0,0 +1,41 @@
+// let name = "John";
+
+// console.log(`Hello, ${name}`);
+
+
+
+
+// const preson = {
+//     name: "John",
+//     age: 30,
+// }
+
+
+// const { name, age } = preson;
+
+
+// console.log(name);
+
+
+
+// function greet(name = "Guest ") {
+//     console.log(`Hello, ${name}`);
+// }
+
+
+// greet();
+
+///spead 
+
+
+// const nums = [1, 2,]
+// const all = [...nums,10];
+// console.log(all);
+
+
+// function sum(...nums) {
+//     return nums.reduce((a, b) => a + b, 0);
+    
+// }
+
+// console.log(sum(1,2,3));
---
+++ js/day12/fetch.js
@@ -0,0 +1,55 @@
+// fetch("https://api.github.com/users/Arun-cloud-dev")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log("Error:", error));
+
+
+
+// navigator.geolocation.getCurrentPosition((position) => {
+//   console.log("Latitude:", position.coords.latitude);
+//   console.log("Longitude:", position.coords.longitude);
+// });
+
+
+
+
+
+// fetch("https://api.github.com/users/Arun-cloud-dev")
+
+//     .then((response) => response.json())
+//     .then(data => {
+//         console.log("Full Object:", data);
+//         console.log("Type of Data :",typeof data);
+
+// })
+
+
+// .catch((error) => console.log("Error:", error));
+
+
+
+
+
+
+
+// 
+
+
+// const obj = JSON.stringify(jsonData);
+// console.log(obj.login);
+
+
+// const jsonData = '{"name":"John","age":20}';
+// const obj = JSON.parse(jsonData);
+// console.log(obj.name); 
+
+
+
+
+
+// fetch("https://api.github.com/users/Arun-cloud-dev")
+//   .then((response) => response.json()) // Converts JSON to JS object
+//   .then((obj) => {
+//     console.log(obj.login); // Access a property (e.g., username)
+//   })
+//   .catch((error) => console.log("Error:", error));
